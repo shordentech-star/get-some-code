f330d3c586983681d81dcdde82f1ff56a52a28b7
Shordentech <shordentech@gmail.com>
2025-08-05 21:45:41 +0530

    commit

---
+++ simple/input your name and other things.py
@@ -0,0 +1,6 @@
+name = input("enter your namr ")
+print(f"Hi you are {name}, right ?")
+
+#this is who to print whatever you want in python
+thing = input("enter what you want to print ")
+print(f"{thing}")
